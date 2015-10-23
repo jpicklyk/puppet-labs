8eb693be259f0b3ac01e17662d4385c7f8c415c3
Jeff Picklyk <jeff.picklyk@sea.samsung.com>
2015-10-23 11:04:16 -0400

    adding firewall rule for puppetdb (8081)

--- dist/profile/manifests/puppetdb.pp
+++ dist/profile/manifests/puppetdb.pp
@@ -17,6 +17,13 @@
 class profile::puppetdb {
   #include ::puppetdb
   notify {'Applying profile: puppetdb':}
+
+  firewall { '100 allow PuppetDB access':
+    dport   => '8081',
+    proto  => tcp,
+    action => accept,
+  }
+
   #Configure puppetdb and its underlying database
   class {'puppetdb':}
   #Configure puppet master to use the puppetdb
