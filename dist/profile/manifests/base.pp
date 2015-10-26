fc33566eb7c137920da1076f3c972d898f837528
Jeff Picklyk <jeff.picklyk@sea.samsung.com>
2015-10-26 11:16:48 -0400

    Removing software share for now.

--- dist/profile/manifests/base.pp
+++ dist/profile/manifests/base.pp
@@ -8,12 +8,12 @@ class profile::base {
     contain profile::iesec
     #contain windows_puppet
     contain profile::win_default_apps
-    contain profile::software_share
+    #contain profile::software_share
     contain profile::powershell
     contain profile::shutdown
     
-    Class['profile::iesec'] -> Class['profile::software_share'] -> Class['profile::powershell'] -> Class['profile::win_default_apps']
-    
+    #Class['profile::iesec'] -> Class['profile::software_share'] -> Class['profile::powershell'] -> Class['profile::win_default_apps']
+    Class['profile::iesec'] -> Class['profile::powershell'] -> Class['profile::win_default_apps']
   } elsif $operatingsystem == 'Ubuntu' {
     
   }
