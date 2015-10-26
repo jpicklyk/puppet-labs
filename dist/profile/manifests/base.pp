36c187b46227df1708d97671238e3ee46890579c
Jeff Picklyk <jeff.picklyk@sea.samsung.com>
2015-10-26 12:47:53 -0400

    Fixing up chocolatey install

--- dist/profile/manifests/base.pp
+++ dist/profile/manifests/base.pp
@@ -10,12 +10,12 @@ class profile::base {
     contain profile::win_default_apps
     #contain profile::software_share
     #Ensure chocolatey is installed and configured
-    contain chocolatey
+    contain ::chocolatey
     contain profile::powershell
     contain profile::shutdown
     
     #Class['profile::iesec'] -> Class['profile::software_share'] -> Class['profile::powershell'] -> Class['profile::win_default_apps']
-    Class['profile::iesec'] -> Class['profile::powershell'] -> Class['chocolatey'] -> Class['profile::win_default_apps']
+    Class['profile::iesec'] -> Class['profile::powershell'] -> Class['::chocolatey'] -> Class['profile::win_default_apps']
   } elsif $operatingsystem == 'Ubuntu' {
     
   }
